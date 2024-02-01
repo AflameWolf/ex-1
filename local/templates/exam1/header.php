bfa736332396f6034e7db7f089e4f0accf9dfa74
AflameWolf <i.demowolf@gmail.com>
2024-02-01 12:03:06 +0300

    [ex1-5] and [ex1-6] tasks.

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -12,7 +12,7 @@ IncludeTemplateLangFile(__FILE__);
     $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH."/css/reset.css" );
 	$APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH."/css/style.css" );
 	$APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH."/css/owl.carousel.css" );
-	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH."./js/jquery.min.js");
+	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH."./js/jquery.min.js"); //метод устарел?
 	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH."./js/owl.carousel.min.js");
 	$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH."./js/scripts.js");
     $APPLICATION->ShowHead();
@@ -81,65 +81,24 @@ IncludeTemplateLangFile(__FILE__);
     <!-- nav -->
     <nav class="nav">
         <div class="inner-wrap">
-            <div class="menu-block popup-wrap">
-                <a href="" class="btn-menu btn-toggle"></a>
-                <div class="menu popup-block">
-                    <ul class="">
-                        <li class="main-page"><a href="">Главная</a>
-                        </li>
-                        <li>
-                            <a href="">Компания</a>
-                            <ul>
-                                <li>
-                                    <a href="">Пункт 1</a>
-                                    <ul>
-                                        <li><a href="">Пункт 1</a>
-                                        </li>
-                                        <li><a href="">Пункт 2</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a href="">Пункт 2</a>
-                                </li>
-                                <li><a href="">Пункт 3</a>
-                                </li>
-                                <li><a href="">Пункт 4</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li><a href="">Новости</a>
-                        </li>
-                        <li>
-                            <a href="">Каталог</a>
-                            <ul>
-                                <li>
-                                    <a href="">Пункт 1</a>
-                                    <ul>
-                                        <li><a href="">Пункт 1</a>
-                                        </li>
-                                        <li><a href="">Пункт 2</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a href="">Пункт 2</a>
-                                </li>
-                                <li><a href="">Пункт 3</a>
-                                </li>
-                                <li><a href="">Пункт 4</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li><a href="">Фотогалерея</a>
-                        </li>
-                        <li><a href="">Партнерам</a>
-                        </li>
-                        <li><a href="">Контакты</a>
-                        </li>
-                    </ul>
-                    <a href="" class="btn-close"></a>
-                </div>
-                <div class="menu-overlay"></div>
-            </div>
+	        <?$APPLICATION->IncludeComponent(
+		        "bitrix:menu",
+		        "top",
+		        Array(
+			        "ALLOW_MULTI_SELECT" => "N",
+			        "CHILD_MENU_TYPE" => "left",
+			        "COMPONENT_TEMPLATE" => "top",
+			        "DELAY" => "N",
+			        "MAX_LEVEL" => "3",
+			        "MENU_CACHE_GET_VARS" => array(),
+			        "MENU_CACHE_TIME" => "3600",
+			        "MENU_CACHE_TYPE" => "N",
+			        "MENU_CACHE_USE_GROUPS" => "Y",
+			        "MENU_THEME" => "site",
+			        "ROOT_MENU_TYPE" => "top",
+			        "USE_EXT" => "Y"
+		        )
+	        );?>
         </div>
     </nav>
     <!-- /nav -->
