e172dbf460d6e8bfbd190d546ecc96c33334be81
AflameWolf <i.demowolf@gmail.com>
2024-02-01 16:13:43 +0300

    [ex1-8] tasks.

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -105,14 +105,23 @@ IncludeTemplateLangFile(__FILE__);
         <nav class="main-menu">
             <div class="item">
                 <div class="title-block">О магазине</div>
-                <ul>
-                    <li><a href="">Отзывы</a>
-                    </li>
-                    <li><a href="">Руководство </a>
-                    </li>
-                    <li><a href="">История</a>
-                    </li>
-                </ul>
+	            <?$APPLICATION->IncludeComponent(
+		            "bitrix:menu",
+		            ".default",
+		            Array(
+			            "ALLOW_MULTI_SELECT" => "N",
+			            "CHILD_MENU_TYPE" => "left",
+			            "COMPONENT_TEMPLATE" => ".default",
+			            "DELAY" => "N",
+			            "MAX_LEVEL" => "1",
+			            "MENU_CACHE_GET_VARS" => array(),
+			            "MENU_CACHE_TIME" => "3600",
+			            "MENU_CACHE_TYPE" => "N",
+			            "MENU_CACHE_USE_GROUPS" => "Y",
+			            "ROOT_MENU_TYPE" => "my_bottom",
+			            "USE_EXT" => "Y"
+		            )
+	            );?>
             </div>
             <div class="item">
                 <div class="title-block">Каталог товаров</div>
